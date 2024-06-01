1ca6ba8574fdd4fb524ccc55fe33101d6408c285
admin <you@example.com>
2024-06-01 11:37:25 +0530

    javacode

---
+++ profile.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+class profile{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter name:");	
+		String name = sc.nextLine();
+		
+		System.out.println("Enter branch:");
+		String branch = sc.nextLine();
+		
+		System.out.println("Enter roll number:");
+		int roll = sc.nextInt();
+		
+		
+		
+		System.out.println("Details:");
+		
+		System.out.println(name);
+		System.out.println(branch);
+		System.out.println(roll);
+		}
+	}
